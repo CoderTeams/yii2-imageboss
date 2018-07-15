0f33b1b52f4f2875f498484a245d2316c1b75ca5
Pablo Nuñez <pablo@coderteams.com>
2018-07-14 23:14:13 -0300

    Fist steps

--- Image.php
+++ Image.php
@@ -1,14 +1,26 @@
 <?php
 
-namespace coderteams\ImageBoss;
+namespace coderteams\imageboss;
+
+use yii\helpers\Html;
 
 /**
  * This is just an example.
  */
 class Image extends \yii\base\Widget
 {
-    public function run()
-    {
-        return "Hello!";
+    public $message;
+
+    public function init(){
+        parent::init();
+        if($this->message===null){
+            $this->message= 'Welcome User';
+        }else{
+            $this->message= 'Welcome '.$this->message;
+        }
+    }
+
+    public function run(){
+        return Html::encode($this->message);
     }
 }
